960fae4ea8b744f9091047ecb3190512cc2a4f7e
redspidersoftware <ochentainueve@gmail.com>
2022-04-19 17:14:51 +0200

    Resume

--- equipo a/Equipo A/Assets/Scripts/Resume.cs
+++ equipo a/Equipo A/Assets/Scripts/Resume.cs
@@ -2,23 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pausa : MonoBehaviour
+public class Resume : MonoBehaviour
 {
-    bool paused = false;
-
     // Start is called before the first frame update
     void Start()
     {
-        paused = false;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("escape"))
-        {        
-            Debug.Log("Pausado");
-            paused = true;
-        }
+        
+    }
+    public void ResumeGame()
+    {
+        Debug.Log("Resume game");
     }
 }
